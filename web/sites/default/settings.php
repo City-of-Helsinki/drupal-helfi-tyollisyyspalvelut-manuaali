8c705ae2f146363dd8194e0b842b42afafa6a4fb
Jyri-Petteri Paloposki <jyri-petteri.paloposki@iki.fi>
2021-11-18 12:19:48 +0200

    Drupal installation

---
+++ web/sites/default/settings.php
@@ -0,0 +1,7 @@
+<?php
+
+$settings['config_sync_directory'] = '../config/sync';
+
+if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+  include $app_root . '/' . $site_path . '/settings.local.php';
+}
