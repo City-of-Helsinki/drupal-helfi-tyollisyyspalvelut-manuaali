3572beebb04c44f28abed8f7b2782b452ffbc235
Jyri-Petteri Paloposki <jyri-petteri.paloposki@iki.fi>
2021-11-23 14:23:43 +0200

    TP-57: Create the Lando environment and basic configuration

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -2,6 +2,19 @@
 
 $settings['config_sync_directory'] = '../config/sync';
 
+$settings['hash_salt'] = 'KrHccV8K1iFq5n-WjbqIQzM40cfaUiBPL2xMXZWKv_AUALCHuLAFrKXsfzV3k8W41v7kjEyj2g';
+
+$databases['default']['default'] = array (
+  'database' => 'drupal8',
+  'username' => 'drupal8',
+  'password' => 'drupal8',
+  'prefix' => '',
+  'host' => 'database.helfimanuska.internal',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
