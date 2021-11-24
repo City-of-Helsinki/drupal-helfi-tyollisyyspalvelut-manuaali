ce25fc0d61bf8a368c953c09e34998d20e916d47
Jyri-Petteri Paloposki <jyri-petteri.paloposki@iki.fi>
2021-11-24 13:06:10 +0200

    TP-57: Platform.sh settings

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -1,5 +1,24 @@
 <?php
 
+/**
+ * @file
+ * Platform.sh example settings.php file for Drupal 8.
+ */
+
+// Default Drupal 8 settings.
+//
+// These are already explained with detailed comments in Drupal's
+// default.settings.php file.
+//
+// See https://api.drupal.org/api/drupal/sites!default!default.settings.php/8
+
+$settings['update_free_access'] = FALSE;
+$settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
+$settings['file_scan_ignore_directories'] = [
+  'node_modules',
+  'bower_components',
+];
+
 $settings['config_sync_directory'] = '../config/sync';
 
 $settings['hash_salt'] = 'KrHccV8K1iFq5n-WjbqIQzM40cfaUiBPL2xMXZWKv_AUALCHuLAFrKXsfzV3k8W41v7kjEyj2g';
@@ -15,6 +34,16 @@ $databases['default']['default'] = array (
   'driver' => 'mysql',
 );
 
+// Automatic Platform.sh settings.
+if (file_exists($app_root . '/' . $site_path . '/settings.platformsh.php')) {
+  include $app_root . '/' . $site_path . '/settings.platformsh.php';
+}
+// Lando settings.
+if (file_exists($app_root . '/' . $site_path . '/settings.lando.php')) {
+  include $app_root . '/' . $site_path . '/settings.lando.php';
+}
+// Local settings. These come last so that they can override anything.
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
+
