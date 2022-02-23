98084ca820f22694a8dbed6fc95f38f5631524ca
Iiro Järvenpää <iirojarvenpaa@Iiros-MacBook-Pro.local>
2022-02-23 10:08:04 +0200

    TP-123 Change logo and fix mobile viewpoints

--- public/themes/custom/palvelumanuaali/dist/svgSprite.js
+++ public/themes/custom/palvelumanuaali/dist/svgSprite.js
@@ -260,7 +260,7 @@ __webpack_require__.r(__webpack_exports__);
 __webpack_require__.r(__webpack_exports__);
 /* harmony default export */ __webpack_exports__["default"] = ({
       id: "logo-small-usage",
-      viewBox: "0 0 24 24",
+      viewBox: "0 0 28 32",
       url: __webpack_require__.p + "../dist/icons.svg#logo-small",
       toString: function () {
         return this.url;
