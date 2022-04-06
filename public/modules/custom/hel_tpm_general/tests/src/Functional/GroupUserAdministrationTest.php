78cf55c756f3fd2a67b3b5ad5bf327f7326c9d2b
Jussi Viitamäki <jussi@kwd.fi>
2022-04-06 09:56:38 +0300

    Installed subgroup module and started configuration

---
+++ public/modules/custom/hel_tpm_general/tests/src/Functional/GroupUserAdministrationTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\Tests\hel_tpm_general\Unit;
+
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * Test description.
+ *
+ * @group hel_tpm_general
+ */
+class GroupUserAdministrationTest extends UnitTestCase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+  }
+
+  /**
+   * Tests something.
+   */
+  public function testSomething() {
+    $this->assertTrue(TRUE, 'This is TRUE!');
+  }
+
+}
