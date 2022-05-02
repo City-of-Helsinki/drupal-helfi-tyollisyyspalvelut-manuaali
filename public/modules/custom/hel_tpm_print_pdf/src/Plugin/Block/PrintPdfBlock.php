ac69474969387c9679c00d2f966468c26001dd95
Iiro Järvenpää <iirojarvenpaa@Iiros-MacBook-Pro.local>
2022-05-02 11:15:29 +0300

    TP-233 pdf printing button placement and theming

---
+++ public/modules/custom/hel_tpm_print_pdf/src/Plugin/Block/PrintPdfBlock.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Drupal\hel_tpm_print_pdf\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+
+/**
+ * Provide a block with entity print link.
+ *
+ * @Block(
+ *   id = "hel_print_pdf_block",
+ *   admin_label = @Translation("Print pdf block"),
+ *   category = @Translation("Print pdf"),
+ * )
+ */
+class PrintPdfBlock extends BlockBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $output = [];
+    $base_url = \Drupal::request()->getRequestUri();
+    $output['#markup'] = '<a class="button log" href="'. $base_url . '?' . $_SERVER['QUERY_STRING'] . '">View PDF</a>';
+    $output['#attached']['library'] = 'hel_tpm_print_pdf/hel_tpm_print_pdf';
+    return $output;
+  }
+}
