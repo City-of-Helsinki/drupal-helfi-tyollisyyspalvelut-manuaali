54540ec86f2863fb64a9212a4dd07ac24efc78a1
Jyri-Petteri Paloposki <jyri-petteri.paloposki@iki.fi>
2022-05-11 13:25:17 +0300

    TP-255: Change service fields based on the migration specs and add support for missing fields' migration.

--- public/modules/custom/hel_tpm_migrate_tools/src/Plugin/migrate/process/TPMContentParagraph.php
+++ public/modules/custom/hel_tpm_migrate_tools/src/Plugin/migrate/process/TPMContentParagraph.php
@@ -24,14 +24,46 @@ class TPMContentParagraph extends ParagraphBase {
   public function transform($value, MigrateExecutableInterface $migrateExecutable, Row $row, $destinationProperty) {
     $paragraphs = [];
 
+    if (!is_array($value)) {
+      $paragraphs[] = $this->createContentParagraph($row, $destinationProperty, NULL, $value);
+    }
+    else {
+      foreach ($value as $key => $body_val) {
+        if (!empty($body_val)) {
+          $paragraphs[] = $this->createContentParagraph($row, $destinationProperty, $this->configuration['titles'][$key], $body_val);
+        }
+      }
+    }
+    return $paragraphs;
+  }
+
+  /**
+   * Create a content paragraph with specified content.
+   *
+   * @param Drupal\migrate\Row $row
+   *  The row being handled.
+   * @param string $destinationProperty
+   *  The property we're currently handling.
+   * @param string|NULL $title
+   *  The title of the paragraph.
+   * @param string $body
+   *  The body of the paragraph.
+   *
+   * @return []
+   *  The paragraph's IDs as necessary to include in a entity reference field.
+   */
+  private function createContentParagraph($row, $destinationProperty, $title, $body) {
     $paragraph = $this->createParagraph($row, $destinationProperty, 'content', 0);
-    $paragraph->field_body = $value;
+    if (!empty($title)) {
+      $paragraph->field_title = $title;
+    }
+    $paragraph->field_body = $body;
     $paragraph->save();
-    $paragraphs[] = [
+
+    return [
       'target_id' => $paragraph->id(),
       'target_revision_id' => $paragraph->getRevisionId(),
     ];
-    return $paragraphs;
   }
 
 }
