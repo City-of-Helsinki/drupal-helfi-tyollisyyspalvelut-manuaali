07eff8bc1bd111c91413411576a369e8ab7713fd
Jyri-Petteri Paloposki <jyri-petteri.paloposki@iki.fi>
2022-06-06 12:45:45 +0300

    PLATTA-3839: Re-add DB credentials to the Openshift config

--- public/sites/default/settings.openshift.php
+++ public/sites/default/settings.openshift.php
@@ -1,2 +1,14 @@
 <?php
 
+$databases['default']['default'] = [
+  'database' => getenv('DRUPAL_DB_NAME'),
+  'username' => getenv('DRUPAL_DB_USER'),
+  'password' => getenv('DRUPAL_DB_PASS'),
+  'prefix' => '',
+  'host' => getenv('DRUPAL_DB_HOST'),
+  'port' => getenv('DRUPAL_DB_PORT') ?: 3306,
+  'namespace' => 'Drupal\Core\Database\Driver\mysql',
+  'driver' => 'mysql',
+];
+
+$settings['hash_salt'] = getenv('DRUPAL_HASH_SALT') ?: '000';
