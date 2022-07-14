fd9f5c9b67038ba3935d6824b477ec4b725bde9e
Jussi Viitamäki <jussi@kwd.fi>
2022-07-14 13:38:52 +0300

    Created add service link which provides group selection

---
+++ public/modules/custom/hel_tpm_general/src/Controller/GroupNodeAddServiceController.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace Drupal\hel_tpm_general\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\Core\Url;
+use Drupal\group\Access\GroupContentCreateEntityAccessCheck;
+use Drupal\group\GroupMembershipLoader;
+use Drupal\hel_tpm_general\Access\GroupNodeCreateAccessService;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Routing\RouterInterface;
+
+class GroupNodeAddServiceController extends ControllerBase implements ContainerInjectionInterface {
+
+  /**
+   * @var \Drupal\group\GroupMembershipLoader
+   */
+  protected $groupMembershipLoader;
+
+  /**
+   * @var \Drupal\group\Access\GroupContentCreateEntityAccessCheck
+   */
+  protected $groupContentCreateEntityAccess;
+
+  /**
+   * @var \Symfony\Component\Routing\RouterInterface
+   */
+  protected $router;
+
+  /**
+   * @var string
+   */
+  protected $plugin_id = 'group_node:service';
+
+  /**
+   * @var \Drupal\hel_tpm_general\Access\GroupNodeCreateAccessService
+   */
+  private $groupNodeCreateAccessService;
+
+  /**
+   * @param \Drupal\group\GroupMembershipLoader $group_membership_loader
+   * @param \Drupal\group\Access\GroupContentCreateEntityAccessCheck $group_content_create_entity_access
+   * @param \Drupal\Core\Path\PathValidatorInterface $path_validator
+   * @param \Symfony\Component\Routing\RouterInterface $router
+   */
+  public function __construct(GroupMembershipLoader $group_membership_loader, RouterInterface $router, GroupNodeCreateAccessService $group_node_create_access_service) {
+    $this->groupMembershipLoader = $group_membership_loader;
+    $this->router = $router;
+    $this->groupNodeCreateAccessService = $group_node_create_access_service;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('group.membership_loader'),
+      $container->get('router.no_access_checks'),
+      $container->get('hel_tpm_general.group_node_add_access_service')
+    );
+  }
+
+  /**
+   * Access callback.
+   *
+   * @return \Drupal\Core\Access\AccessResultAllowed|\Drupal\Core\Access\AccessResultForbidden
+   */
+  public function access() {
+    return $this->groupNodeCreateAccessService->hasAccess($this->currentUser(), $this->plugin_id);
+  }
+
+  /**
+   * Title callback.
+   *
+   * @return \Drupal\Core\StringTranslation\TranslatableMarkup
+   */
+  public function title() {
+    return $this->t('Create service for group');
+  }
+
+  /**
+   * Add service list method.
+   *
+   * @return array
+   */
+  public function addServiceList() : array {
+    $groups = $this->userGroups();
+    return [
+      '#theme' => 'item_list',
+      '#title' => 'Select group',
+      '#items' => $this->generateLinks($groups)
+    ];
+  }
+
+  /**
+   * Group selection callback when creating a new service.
+   *
+   * @param $groups
+   *
+   * @return array
+   */
+  private function generateLinks($groups) {
+    $links = [];
+    if (empty($groups)) {
+      return $links;
+    }
+    foreach ($groups as $group) {
+      if (!$this->groupNodeCreateAccessService->hasCreateServiceAccess($group, $this->plugin_id, $this->currentUser())) {
+        continue;
+      }
+      $links[] = [
+        '#type' => 'link',
+        '#title' => $group->label(),
+        '#url' => Url::fromUserInput($this->buildPath($group, $this->plugin_id))
+      ];
+    }
+
+    return $links;
+  }
+  /**
+   * @param $group
+   * @param $plugin_id
+   *
+   * @return string
+   */
+  private function buildPath($group, $plugin_id) {
+    return sprintf('/group/%s/content/create/%s', $group->id(), $plugin_id);
+  }
+
+  /**
+   * @return array
+   */
+  protected function userGroups() : array {
+    $groups = [];
+    $memberships = $this->groupMembershipLoader->loadByUser($this->currentUser);
+    if (empty($memberships)) {
+      return $groups;
+    }
+
+    foreach ($memberships as $membership) {
+      $groups[] = $membership->getGroup();
+    }
+    return $groups;
+  }
+}
