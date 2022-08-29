ece7e728d0abbcead4e6e19629ce06f4643a8234
Iiro Järvenpää <iirojarvenpaa@Iiros-MacBook-Pro.local>
2022-08-29 11:14:49 +0300

    TP-239 refactor service form stepper

---
+++ public/modules/custom/hel_tpm_forms/src/Plugin/Block/HelTpmStepsBlock.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Drupal\hel_tpm_forms\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use \Drupal\file\Entity\File;
+
+/**
+ * Provides a landing content block.
+ *
+ * @Block(
+ *   id = "hel_tpm_steps",
+ *   admin_label = @Translation("Hel TPM Steps"),
+ *   category = @Translation("Custom"),
+ * )
+ */
+class HelTpmStepsBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $content = [];
+    $render_array = [
+      '#theme' => 'hel_tpm_steps',
+      '#content' => $content,
+    ];
+
+    return $render_array;
+  }
+
+}
