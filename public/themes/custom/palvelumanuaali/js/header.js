4dfb88995d546c6429cf01f6a6cee2240ee6e61d
Iiro Järvenpää <iirojarvenpaa@Iiros-MacBook-Pro.local>
2022-10-10 12:25:55 +0300

    TP-423 Change sidebar to be full height and header to be fixed

---
+++ public/themes/custom/palvelumanuaali/js/header.js
@@ -0,0 +1,22 @@
+(function ($, Drupal, window) {
+  'use strict';
+  // Example of Drupal behavior loaded.
+  Drupal.behaviors.exampleJS = {
+    attach: function (context, settings) {
+      if (typeof context['location'] !== 'undefined') { // Only fire on document load.
+        const nav = $("#page-header");
+        var lastScrollY = 0;
+        $(window).scroll(function () {
+          var currentScrollY = $(this).scrollTop();
+          if (lastScrollY < currentScrollY && currentScrollY > 50) {
+            nav.addClass("header--hidden");
+          } else {
+            nav.removeClass("header--hidden");
+          }
+            lastScrollY = currentScrollY;
+        });
+      }
+    }
+  };
+
+})(jQuery, Drupal, this);
