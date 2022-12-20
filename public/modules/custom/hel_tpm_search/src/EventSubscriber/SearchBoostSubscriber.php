c9d6fe523137007237684b0402f06988dbec5ae9
Jussi Viitamäki <jussi@kwd.fi>
2022-12-20 11:06:28 +0200

    Fixed elastic search subscriber error

--- public/modules/custom/hel_tpm_search/src/EventSubscriber/SearchBoostSubscriber.php
+++ public/modules/custom/hel_tpm_search/src/EventSubscriber/SearchBoostSubscriber.php
@@ -19,9 +19,7 @@ class SearchBoostSubscriber implements EventSubscriberInterface {
    *   The event names to listen for, and the methods that should be executed.
    */
   public static function getSubscribedEvents() {
-    return [
-      BuildSearchParamsEvent::BUILD_QUERY => 'buildSearchParamsEvent',
-    ];
+    return [];
   }
 
   /**
@@ -31,28 +29,6 @@ class SearchBoostSubscriber implements EventSubscriberInterface {
    *   ES search query prepare event.
    */
   public function buildSearchParamsEvent(BuildSearchParamsEvent $event) {
-    $q = $event->getElasticSearchParams();
-
-    $query = $q['body'];
-
-    $q['body']['query'] = [
-      'function_score' => [
-        'query' => $query['query'],
-        'boost_mode' => 'sum',
-        'functions' => [
-          [
-            'field_value_factor' => [
-              'field' => 'hel_tpm_priority_boost',
-              'factor' => 1.5,
-              'missing' => 1,
-              'modifier' => "none"
-            ],
-          ]
-        ]
-      ]
-    ];
-
-    $event->setElasticSearchParams($q);
   }
 
 }
