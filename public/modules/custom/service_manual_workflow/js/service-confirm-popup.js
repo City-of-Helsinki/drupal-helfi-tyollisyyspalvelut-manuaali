00aadcebce637b703b5d1d9f3eddd43da61dde51
Jussi Viitamäki <jussi@kwd.fi>
2023-07-06 11:23:16 +0300

    TP-910 updated popup texts

--- public/modules/custom/service_manual_workflow/js/service-confirm-popup.js
+++ public/modules/custom/service_manual_workflow/js/service-confirm-popup.js
@@ -6,25 +6,26 @@
       // call to function to open popup
       $("#edit-submit").click(function (e) {
         e.preventDefault();
-        let message = confirmMessage(context);
+        let message = getConfirmMessage(context);
         if ($('#edit-title-0-value').val() != '') {
           title = $('#edit-title-0-value').val();
         }
-        confirmPopup(title);
+        confirmPopup(title, message);
       });
 
-      function confirmMessage(context) {
-        console.log(context);
-      }
-
-      function confirmPopup(title = "") {
-        var content = '<div>Are you sure you want save <b>' + title + '</b>?</div>';
+      /**
+       *
+       * @param title
+       * @param message
+       */
+      function confirmPopup(title = "", message = "") {
+        var content = '<div class="desc">' + message + '</div>';
         confirmationDialog = Drupal.dialog(content, {
           dialogClass: 'confirm-dialog',
           resizable: true,
           closeOnEscape: false,
           width: 600,
-          title: "Saving Confirmation",
+          title: title,
           buttons: [
             {
               text: 'Confirm',
@@ -42,6 +43,42 @@
           ],
         });
         confirmationDialog.showModal();
+      }
+
+      /**
+       * Get confirm message for
+       * @param context
+       * @returns {*}
+       */
+      function getConfirmMessage(context) {
+        let selector = 'edit-moderation-state-0';
+        let moderationStateWidget = $('div[data-drupal-selector="' + selector + '"]', context);
+        let state = getState(moderationStateWidget, selector);
+        return fetchMessageForState(state);
+      }
+
+      /**
+       * Fetch message for selected state.
+       *
+       * @param state
+       * @returns {*}
+       */
+      function fetchMessageForState(state) {
+        if (settings.service_manual_workflow.popup_settings[state] === '') {
+          return
+        }
+        return settings.service_manual_workflow.popup_settings[state];
+      }
+
+      /**
+       * Get selected state.
+       *
+       * @param widget
+       * @param selector
+       * @returns {*|string|jQuery}
+       */
+      function getState(widget, selector) {
+        return $("select", widget).val();
       }
     }
   }
