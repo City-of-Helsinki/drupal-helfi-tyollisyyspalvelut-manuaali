3b003484e3634773a397b95a6095ae0b67a567f8
Jussi Viitamäki <jussi@kwd.fi>
2023-10-06 14:43:31 +0300

    TP-1046 fixed hel_tpm_group Drupal 10 compatibility issues

--- public/modules/custom/hel_tpm_group_invite/src/EventSubscriber/BulkInvitationRouteSubscriber.php
+++ public/modules/custom/hel_tpm_group_invite/src/EventSubscriber/BulkInvitationRouteSubscriber.php
@@ -56,7 +56,7 @@ class BulkInvitationRouteSubscriber extends RouteSubscriberBase {
   /**
    * {@inheritdoc}
    */
-  public static function getSubscribedEvents() {
+  public static function getSubscribedEvents(): array {
     $events = parent::getSubscribedEvents();
 
     // Use a lower priority than \Drupal\views\EventSubscriber\RouteSubscriber
