44b4216aa780787eaa3d73b56ec158c158c5f39f
Jussi Viitamäki <jussi@kwd.fi>
2023-10-06 08:58:30 +0300

    TP-1039 Fixed D10 compatibility issues

--- public/modules/custom/hel_tpm_url_shortener/src/Form/ShortenerredirectForm.php
+++ public/modules/custom/hel_tpm_url_shortener/src/Form/ShortenerredirectForm.php
@@ -4,6 +4,7 @@ namespace Drupal\hel_tpm_url_shortener\Form;
 
 use Drupal\Core\Entity\ContentEntityForm;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Render\RendererInterface;
 
 /**
  * Form controller for the shortenerredirect entity edit forms.
@@ -20,7 +21,7 @@ class ShortenerredirectForm extends ContentEntityForm {
     $link = $entity->toLink($this->t('View'))->toRenderable();
 
     $message_arguments = ['%label' => $this->entity->label()];
-    $logger_arguments = $message_arguments + ['link' => render($link)];
+    $logger_arguments = $message_arguments + ['link' => RendererInterface::render($link)];
 
     if ($result == SAVED_NEW) {
       $this->logger('hel_tpm_url_shortener')->notice('Created new shortenerredirect %label', $logger_arguments);
