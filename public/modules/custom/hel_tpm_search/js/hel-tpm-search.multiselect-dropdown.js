fc89fd1afef48c76351ed347c648a2537135f83b
IiroJarv <74237192+IiroJarv@users.noreply.github.com>
2023-12-01 14:16:19 +0200

    Merge pull request #553 from City-of-Helsinki/task/TP-1186_search_filter_debug
    Task/tp 1186 search filter debug

--- public/modules/custom/hel_tpm_search/js/hel-tpm-search.multiselect-dropdown.js
+++ public/modules/custom/hel_tpm_search/js/hel-tpm-search.multiselect-dropdown.js
@@ -10,16 +10,26 @@
           });
           $(label).hide();
         });
-        // HOT FIX: reset default functionality
-        // $('.form-item-field-free-service select').each(function () {
-        //   $(this).removeAttr('multiple');
-        // });
-        // $('.form-item-field-free-service input:checked').on( 'click', function(event) {
-        //   event.stopPropagation();
-        //   event.preventDefault();
-        //   $(this).closest('form-item__dropdown').find('select').val('');
-        //   $(this).closest('form').find('.cost-reset input[id^="edit-reset--"]').click();
-        // });
+        // Handle clicking not selected input to mimic radiobuttons
+        $('.form-item-field-free-service input:not(:checked)').on( 'click', function(event) {
+          event.preventDefault();
+          let value = $(this).val();
+          if (value == 1 ) {
+            $(this).closest('.form-item__dropdown').find('select:selected').val('');
+            $(this).closest('.form-item__dropdown').find('select').val('1');
+          } else {
+            $(this).closest('.form-item__dropdown').find('select:selected').val('');
+            $(this).closest('.form-item__dropdown').find('select').val('2');
+          }
+           $(this).closest('form').find('.text-search-wrapper .form-submit').click();
+        });
+
+        $('.form-item-field-free-service input:checked').on( 'click', function(event) {
+          event.stopPropagation();
+          event.preventDefault();
+          $(this).closest('form-item__dropdown').find('select').val('');
+          $(this).closest('form').find('.cost-reset input[id^="edit-reset--"]').click();
+        });
       });
     }
   };
