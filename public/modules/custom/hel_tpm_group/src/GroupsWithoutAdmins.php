be53839535003eb081b9d0d2fa636d0dcb29756a
Jussi Viitamäki <jussi.viitamaki@citrus.fi>
2024-03-05 11:18:10 +0200

    Created service to fetch groups without group admins

--- public/modules/custom/hel_tpm_group/src/GroupsWithoutAdmins.php
+++ public/modules/custom/hel_tpm_group/src/GroupsWithoutAdmins.php
@@ -4,6 +4,7 @@ namespace Drupal\hel_tpm_group;
 
 use Drupal\Core\Database\Connection;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\group\Entity\GroupMembership;
 
 /**
  * @todo Add class description.
@@ -12,7 +13,7 @@ final class GroupsWithoutAdmins {
 
   private static $roles = [
     'service_provider-group_admin',
-    'organisation-administration'
+    'organisation-administrator'
   ];
 
   /**
@@ -26,15 +27,42 @@ final class GroupsWithoutAdmins {
   /**
    * @todo Add method description.
    */
-  public function groupsWithoutAdmins(): void {
+  public function groupsWithoutAdmins(): array {
     $groups = $this->entityTypeManager->getStorage('group')->getQuery()
       ->accessCheck(FALSE)
       ->execute();
-
-    $this->database->select('')
-    foreach($groups as $group) {
-
+    foreach($groups as $gid => $group) {
+      if (!$this->groupHasAdminUsers($group, self::$roles)) {
+        continue;
+      }
+      unset($groups[$gid]);
     }
+    return $groups;
   }
 
+  /**
+   * Group has members with roles method.
+   *
+   * @param $gid
+   *
+   * @return array|\Drupal\Core\Entity\EntityInterface[]
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   */
+  private function groupHasAdminUsers($gid, $roles, $count = FALSE) {
+    $storage = \Drupal::entityTypeManager()->getStorage('group_content');
+
+    $query = $storage->getQuery()
+      ->accessCheck(FALSE)
+      ->condition('gid', $gid)
+      ->condition('plugin_id', 'group_membership');
+
+    if (isset($roles)) {
+      $query->condition('group_roles', (array) $roles, 'IN');
+    }
+
+    $query->count();
+    $count = $query->execute();
+    return $count <= 0 ? FALSE : TRUE;
+  }
 }
