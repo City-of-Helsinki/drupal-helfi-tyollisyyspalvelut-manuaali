645891a95c517ba6afee2929f67792436290d3ce
jviitamaki <jussi.viitamaki@citrus.fi>
2024-06-11 11:23:12 +0300

    Started to work on select all button

--- public/modules/custom/hel_tpm_search/js/hel-tpm-search.taxonomy-hierarchy-select.js
+++ public/modules/custom/hel_tpm_search/js/hel-tpm-search.taxonomy-hierarchy-select.js
@@ -2,6 +2,15 @@
   Drupal.behaviors.helTpmSearchTaxonomyHierarchySelect = {
     attach: function (context, settings) {
       // Handles clicking of unchecked taxonomy checkbox
+      $(document).ready(function () {
+        let nestingLevel = ".hierarchy-select-buttons .form-checkboxes ul.nesting-level-1";
+        let allInput = "<li><div class='form-item form-item-checkbox select-all'>" +
+          "<input class='hierarchy-select-buttons form-checkbox' type='checkbox' value='all'></input>" +
+          "<label>" +  Drupal.t("Select all") + "</label>" +
+          "</div></li>";
+        $(once("nesting-all", nestingLevel)).prepend(allInput);
+
+      })
       handleFirstLevelSelection();
       handleSecondLevelSelection();
 
@@ -16,6 +25,9 @@
           lastSelection.prop("checked", false);
           lastSelection.parent().removeClass('highlight');
           $(this).prop("checked", true);
+
+          // Add highlight class as default for select-all element in next nesting level.
+          $('ul.nesting-level-1 .select-all').addClass('highlight');
         });
         // Handles clicking of checked taxonomy checkbox
         $('input:checked', firstLevel).off().click(function() {
@@ -29,11 +41,8 @@
        * Second level selection.
        */
       function handleSecondLevelSelection() {
-        let nestingLevel = ".hierarchy-select-buttons .form-checkboxes ul.nesting-level-0";
+        let nestingLevel = ".hierarchy-select-buttons .form-checkboxes ul.nesting-level-1";
         let secondLevel = $("li > .form-item", nestingLevel);
-
-        let allInput = "<li><input>" + t("Select all") + "</input></li>";
-        $(nestingLevel).prepend(allInput);
 
         $('input:not(:checked)', secondLevel).off().click(function() {
           let lastSelection = $(this).closest('ul.nesting-level-1').find('input:checked');
@@ -44,7 +53,7 @@
         // Handles clicking of checked taxonomy checkbox
         $('input:checked', secondLevel).off().click(function() {
           $(this).prop("checked", false);
-          $(this).parent().toggleClass('highlight');
+          $(this).parent().removeClass('highlight');
         });
       }
     }
