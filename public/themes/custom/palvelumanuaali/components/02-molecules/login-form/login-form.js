a0682deca1bdb9fbdb57a793f6c5d43a179113c9
Iiro Järvenpää <iirojarvenpaa@Iiros-MacBook-Pro.local>
2024-08-09 07:18:52 +0300

    TP-1381 Add and theme slide in login page

---
+++ public/themes/custom/palvelumanuaali/components/02-molecules/login-form/login-form.js
@@ -0,0 +1,36 @@
+(function ($, Drupal, window) {
+  'use strict';
+Drupal.behaviors.loginForm = {
+      attach: function (context, settings) {
+
+      let loginBlock = $('.header__secondary .block-user-login-block');
+      let header = $('.header__primary');
+      let additionalMenu = $('.header__primary .additional-links-menu-nav');
+      let body = $('body');
+      let toggleLogin = $('.header__secondary .block-user >.btn-menu');
+      let toggleInnerLogin = $('.header__secondary .slide-in-loginform .btn-menu-close');
+      let sidebarLogin = $('.header__secondary .slide-in-loginform');
+
+    if (typeof loginBlock !== 'undefined' || menu == null) {
+
+      // Mobile Menu Show/Hide.
+      toggleLogin[0].addEventListener('click', (e) => {
+        body[0].classList.toggle('no-scroll');
+        sidebarLogin.removeClass('slide-out');
+        sidebarLogin[0].classList.toggle('slide-in');
+        e.preventDefault();
+
+      });
+
+      // Mobile Menu Show/Hide.
+      toggleInnerLogin[0].addEventListener('click', (e) => {
+        sidebarLogin[0].classList.toggle('slide-in');
+        sidebarLogin[0].classList.toggle('slide-out');
+        body[0].classList.toggle('no-scroll');
+        setTimeout(function() { sidebarLogin.removeClass('slide-out'); }, 1000);
+        e.preventDefault();
+      });
+    }
+  },
+};
+})(jQuery, Drupal, this);
