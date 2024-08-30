a31a2b612af3659de5bc8ece00981260167c40a6
jviitamaki <jussi.viitamaki@citrus.fi>
2024-08-30 13:48:02 +0300

    TP-1198 Added service published row tests.

--- public/modules/custom/hel_tpm_service_stats/tests/src/Kernel/RevisionHistoryTest.php
+++ public/modules/custom/hel_tpm_service_stats/tests/src/Kernel/RevisionHistoryTest.php
@@ -34,6 +34,7 @@ final class RevisionHistoryTest extends GroupKernelTestBase {
     'group',
     'ggroup',
     'ggroup_role_mapper',
+    'views',
     'field_permissions',
     'flexible_permissions',
     'service_manual_workflow_service_test',
@@ -101,4 +102,48 @@ final class RevisionHistoryTest extends GroupKernelTestBase {
     $this->assertEmpty($previous_rev);
   }
 
+  /**
+   * Test service revision row.
+   *
+   * @return void
+   *  -
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  public function testServiceRevisionRow() {
+    $user = $this->createUser([], NULL, TRUE);
+    $node = $this->createNode([
+      'type' => 'service',
+      'moderation_state' => 'draft',
+    ]);
+    $service_row_storage = $this->entityTypeManager->getStorage('service_published_row');
+
+    $this->setCurrentUser($user);
+    $this->setNodeModerationState($node, 'ready_to_publish');
+
+    $published_revisions = $this->revisionHistoryService->getPublishedRevisions();
+    $this->assertEmpty($published_revisions);
+
+    $this->setNodeModerationState($node, 'published');
+    $published_revisions = $this->revisionHistoryService->getPublishedRevisions();
+
+    $this->assertCount(1, $published_revisions);
+    $this->assertEquals(3, $published_revisions[0]->revision_id);
+    $this->assertEquals('published', $published_revisions[0]->moderation_state);
+
+    $previous_rev = $this->revisionHistoryService->getPreviousRevision($published_revisions[0]);
+    $revision = $service_row_storage->load($previous_rev->id);
+    $this->assertEquals(0, $revision->publish_interval->getValue()[0]['value']);
+
+    $this->setNodeModerationState($node, 'ready_to_publish');
+    $node->setRevisionCreationTime(strtotime('now +3 days'));
+    $this->setNodeModerationState($node, 'published');
+
+    $revision = $service_row_storage->loadByProperties(['publish_vid' => $node->getRevisionId()]);
+    $revision = reset($revision);
+
+    $this->assertEquals(3, $revision->publish_interval->getValue()[0]['value']);
+  }
 }
