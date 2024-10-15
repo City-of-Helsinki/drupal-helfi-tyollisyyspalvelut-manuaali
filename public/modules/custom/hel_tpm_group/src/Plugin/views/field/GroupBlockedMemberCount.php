023c548b88250180fa8c130b548760350d31c099
Tapio Pietinsalo <tapio.pietinsalo@citrus.fi>
2024-10-15 09:10:11 +0300

    TP-1001 Add group overview page for admins

---
+++ public/modules/custom/hel_tpm_group/src/Plugin/views/field/GroupBlockedMemberCount.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\hel_tpm_group\Plugin\views\field;
+
+use Drupal\group\Entity\Group;
+use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\views\ResultRow;
+
+/**
+ * A handler to provide the number of blocked group members.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("hel_tpm_group_blocked_member_count")
+ */
+class GroupBlockedMemberCount extends FieldPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function render(ResultRow $values) {
+    $activeMembers = 0;
+    if ($values->_entity instanceof Group) {
+      $members = $values->_entity->getMembers();
+      foreach ($members as $member) {
+        if ($member->getUser()->isBlocked()) {
+          $activeMembers++;
+        }
+      }
+    }
+    return $activeMembers;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {}
+
+  /**
+   * {@inheritdoc}
+   */
+  public function clickSort($order) {}
+
+}
