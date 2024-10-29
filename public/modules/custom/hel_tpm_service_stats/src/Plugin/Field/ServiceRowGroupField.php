43979b2ed264f9ca1cc155fa73c0a44e78cfd11e
jviitamaki <jussi.viitamaki@citrus.fi>
2024-10-29 07:30:27 +0200

    TP-1512 Fixed phpcs issues

--- public/modules/custom/hel_tpm_service_stats/src/Plugin/Field/ServiceRowGroupField.php
+++ public/modules/custom/hel_tpm_service_stats/src/Plugin/Field/ServiceRowGroupField.php
@@ -1,13 +1,20 @@
 <?php
+
 namespace Drupal\hel_tpm_service_stats\Plugin\Field;
 
 use Drupal\Core\Field\EntityReferenceFieldItemList;
 use Drupal\Core\TypedData\ComputedItemListTrait;
 use Drupal\group\Entity\GroupMembership;
 
+/**
+ * Service row group field.
+ */
 class ServiceRowGroupField extends EntityReferenceFieldItemList {
   use ComputedItemListTrait;
 
+  /**
+   * {@inheritdoc}
+   */
   protected function computeValue() {
     $storage = \Drupal::entityTypeManager()->getStorage('node');
     $entity = $this->getEntity();
