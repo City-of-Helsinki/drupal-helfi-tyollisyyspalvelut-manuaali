480c8d44fc4662e839ef60c24d3b7810623090b3
Tapio Pietinsalo <tapio.pietinsalo@citrus.fi>
2024-10-30 10:14:29 +0200

    TP-1001 Show active and blocked member counts

--- public/modules/custom/hel_tpm_group/src/Plugin/views/field/GroupActiveMemberCount.php
+++ public/modules/custom/hel_tpm_group/src/Plugin/views/field/GroupActiveMemberCount.php
@@ -2,12 +2,12 @@
 
 namespace Drupal\hel_tpm_group\Plugin\views\field;
 
-use Drupal\group\Entity\Group;
 use Drupal\views\Plugin\views\field\FieldPluginBase;
 use Drupal\views\ResultRow;
+use Drupal\views\Views;
 
 /**
- * A handler to provide the number of active group members.
+ * Provides the number of active group members.
  *
  * @ingroup views_field_handlers
  *
@@ -18,27 +18,57 @@ class GroupActiveMemberCount extends FieldPluginBase {
   /**
    * {@inheritdoc}
    */
-  public function render(ResultRow $values) {
-    $activeMembers = 0;
-    if ($values->_entity instanceof Group) {
-      $members = $values->_entity->getMembers();
-      foreach ($members as $member) {
-        if (!$member->getUser()->isBlocked()) {
-          $activeMembers++;
-        }
-      }
-    }
-    return $activeMembers;
+  public function query() {
+    $this->ensureMyTable();
+    $this->field_alias = $this->query->addField('active_members', 'active_count');
   }
 
   /**
    * {@inheritdoc}
    */
-  public function query() {}
+  public function ensureMyTable() {
+    if (!isset($this->tableAlias)) {
+      $configuration = [
+        'table formula' => $this->activeUsersTableFormula(),
+        'field' => 'group_id',
+        'left_table' => 'groups_field_data',
+        'left_field' => 'id',
+        'operator' => '=',
+      ];
+      /** @var \Drupal\views\Plugin\views\join\JoinPluginBase $join */
+      $join = Views::pluginManager('join')->createInstance('standard', $configuration);
+      $this->tableAlias = $this->query->addRelationship('active_members', $join, 'groups_field_data');
+    }
+    return $this->tableAlias;
+  }
 
   /**
    * {@inheritdoc}
    */
-  public function clickSort($order) {}
+  public function render(ResultRow $values) {
+    $value = $this->getValue($values);
+    if ($value === NULL) {
+      $value = 0;
+    }
+    return $this->sanitizeValue($value);
+  }
+
+  /**
+   * Get table formula for active member count.
+   *
+   * @return mixed
+   *   Select query.
+   */
+  private function activeUsersTableFormula(): mixed {
+    /** @var \Drupal\Core\Database\Query\SelectInterface $select */
+    $select = $this->view->query->getConnection()->select('group_relationship_field_data', 'grfd');
+    $select->addField('grfd', 'gid', 'group_id');
+    $select->condition('grfd.plugin_id', 'group_membership');
+    $select->addJoin('INNER', 'users_field_data', 'ufd', 'ufd.uid = grfd.entity_id');
+    $select->condition('ufd.status', 1);
+    $select->addExpression("COUNT(ufd.status)", 'active_count');
+    $select->groupBy('group_id');
+    return $select;
+  }
 
 }
--- public/modules/custom/hel_tpm_group/src/Plugin/views/field/GroupBlockedMemberCount.php
+++ public/modules/custom/hel_tpm_group/src/Plugin/views/field/GroupBlockedMemberCount.php
@@ -2,12 +2,12 @@
 
 namespace Drupal\hel_tpm_group\Plugin\views\field;
 
-use Drupal\group\Entity\Group;
 use Drupal\views\Plugin\views\field\FieldPluginBase;
 use Drupal\views\ResultRow;
+use Drupal\views\Views;
 
 /**
- * A handler to provide the number of blocked group members.
+ * Provides the number of blocked group members.
  *
  * @ingroup views_field_handlers
  *
@@ -18,27 +18,58 @@ class GroupBlockedMemberCount extends FieldPluginBase {
   /**
    * {@inheritdoc}
    */
-  public function render(ResultRow $values) {
-    $activeMembers = 0;
-    if ($values->_entity instanceof Group) {
-      $members = $values->_entity->getMembers();
-      foreach ($members as $member) {
-        if ($member->getUser()->isBlocked()) {
-          $activeMembers++;
-        }
-      }
-    }
-    return $activeMembers;
+  public function query() {
+    $this->ensureMyTable();
+    $this->field_alias = $this->query->addField('blocked_members', 'blocked_count');
   }
 
   /**
    * {@inheritdoc}
    */
-  public function query() {}
+  public function ensureMyTable() {
+    if (!isset($this->tableAlias)) {
+      $configuration = [
+        'table formula' => $this->blockedUsersTableFormula(),
+        'field' => 'group_id',
+        'left_table' => 'groups_field_data',
+        'left_field' => 'id',
+        'operator' => '=',
+      ];
+      /** @var \Drupal\views\Plugin\views\join\JoinPluginBase $join */
+      $join = Views::pluginManager('join')->createInstance('standard', $configuration);
+      $this->tableAlias = $this->query->addRelationship('blocked_members', $join, 'groups_field_data');
+    }
+
+    return $this->tableAlias;
+  }
 
   /**
    * {@inheritdoc}
    */
-  public function clickSort($order) {}
+  public function render(ResultRow $values) {
+    $value = $this->getValue($values);
+    if ($value === NULL) {
+      $value = 0;
+    }
+    return $this->sanitizeValue($value);
+  }
+
+  /**
+   * Get table formula for blocked member count.
+   *
+   * @return mixed
+   *   Select query.
+   */
+  private function blockedUsersTableFormula(): mixed {
+    /** @var \Drupal\Core\Database\Query\SelectInterface $select */
+    $select = $this->view->query->getConnection()->select('group_relationship_field_data', 'grfd');
+    $select->addField('grfd', 'gid', 'group_id');
+    $select->condition('grfd.plugin_id', 'group_membership');
+    $select->addJoin('INNER', 'users_field_data', 'ufd', 'ufd.uid = grfd.entity_id');
+    $select->condition('ufd.status', 0);
+    $select->addExpression("COUNT(ufd.status)", 'blocked_count');
+    $select->groupBy('group_id');
+    return $select;
+  }
 
 }
