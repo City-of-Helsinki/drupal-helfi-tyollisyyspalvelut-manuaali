e73d711a40d1e04abe1aa5708a158f4587f30d74
Iiro Järvenpää <iirojarvenpaa@Iiros-MacBook-Pro.local>
2025-04-01 09:38:51 +0300

    TP-1733 fix login button visibility bug

--- public/themes/custom/palvelumanuaali/js/login.js
+++ public/themes/custom/palvelumanuaali/js/login.js
@@ -4,13 +4,11 @@
   Drupal.behaviors.searchCollapse = {
     attach: function (context, settings) {
         let loginBlock = $(".header__primary >.block-user-login-block");
-    //    let userBlock = $(".header__primary >.menu--account");
         if (loginBlock.length === 0) {
           return
         }
 
         $(loginBlock).appendTo(".main-menu--sub-1");
-//$(userBlock).appendTo(".main-menu--sub-1");
       }
   };
 })(jQuery, Drupal, this);
