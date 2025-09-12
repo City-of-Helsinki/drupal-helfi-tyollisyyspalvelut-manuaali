be38463d9eec7dc02c39b8c1abb07aa84a4c6750
jviitamaki <jussi.viitamaki@citrus.fi>
2025-09-12 12:51:40 +0300

    TP-1870 Refactored service archival

--- public/modules/custom/service_manual_workflow/src/Plugin/QueueWorker/ServiceArchivalQueue.php
+++ public/modules/custom/service_manual_workflow/src/Plugin/QueueWorker/ServiceArchivalQueue.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace Drupal\service_manual_workflow\Plugin\QueueWorker;
 
+use Drupal\Component\Datetime\Time;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Queue\QueueWorkerBase;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\node\NodeInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -20,6 +23,8 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  */
 final class ServiceArchivalQueue extends QueueWorkerBase implements ContainerFactoryPluginInterface {
 
+  use StringTranslationTrait;
+
   /**
    * Constructs a new ServiceArchivalService instance.
    */
@@ -28,6 +33,7 @@ final class ServiceArchivalQueue extends QueueWorkerBase implements ContainerFac
     $plugin_id,
     $plugin_definition,
     private readonly EntityTypeManagerInterface $entityTypeManager,
+    private readonly Time $time,
   ) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
   }
@@ -41,6 +47,7 @@ final class ServiceArchivalQueue extends QueueWorkerBase implements ContainerFac
       $plugin_id,
       $plugin_definition,
       $container->get('entity_type.manager'),
+      $container->get('datetime.time')
     );
   }
 
@@ -52,15 +59,29 @@ final class ServiceArchivalQueue extends QueueWorkerBase implements ContainerFac
     $languages = $node->getTranslationLanguages();
     foreach ($languages as $language) {
       $translation = $node->getTranslation($language->getId());
-      $translation->set('moderation_state', 'archived');
-      if ($translation->isPublished()) {
-        $translation->set('status', 0);
-      }
-      $translation->setNewRevision(TRUE);
-      $translation->setRevisionCreationTime(\Drupal::time()->getRequestTime());
-      $translation->setRevisionUserId(1);
-      $translation->save();
+      $this->setServiceArchived($translation);
     }
+  }
+
+  /**
+   * Sets the given service node to the archived state.
+   *
+   * @param \Drupal\node\NodeInterface $node
+   *   The node entity to be archived.
+   *
+   * @return void
+   *   This method does not return any value.
+   */
+  protected function setServiceArchived(NodeInterface $node) {
+    if ($node->isDefaultTranslation()) {
+      $node->setRevisionUserId(1);
+      $node->setChangedTime($this->time->getRequestTime());
+      $node->setRevisionLogMessage($this->t('Set automatically to archived'));
+      $node->setRevisionCreationTime($this->time->getRequestTime());
+    }
+    $node->setRevisionTranslationAffected(TRUE);
+    $node->set('moderation_state', 'archived');
+    $node->save();
   }
 
 }
