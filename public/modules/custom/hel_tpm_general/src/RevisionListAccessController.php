9f16820a3503ea0651292a2523391a23940037c1
jviitamaki <jussi.viitamaki@citrus.fi>
2025-10-15 12:44:45 +0300

    TP-1955 Added custom access check for node revision history

---
+++ public/modules/custom/hel_tpm_general/src/RevisionListAccessController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\hel_tpm_general;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\node\NodeInterface;
+
+/**
+ * Provides access control for the revision list of nodes.
+ */
+final class RevisionListAccessController {
+
+  /**
+   * Checks if the user has update access to the given node.
+   *
+   * @param NodeInterface $node
+   *   The node to check access for.
+   *
+   * @return \Drupal\Core\Access\AccessResult
+   *   The access result indicating whether the user is allowed or denied.
+   */
+  public function access(NodeInterface $node) {
+    $access = $node->access('update');
+    return AccessResult::allowedIf($access);
+  }
+}
