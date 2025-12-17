d0006fdbb691c9d73edf7d5921203b662221af49
jviitamaki <jussi.viitamaki@citrus.fi>
2025-12-17 13:14:04 +0200

    TP-2024 Fixed loop logic

--- scripts/flag-cleaner.php
+++ scripts/flag-cleaner.php
@@ -5,12 +5,13 @@
  */
 $limit = 1000;
 $database = \Drupal::database();
+$count = 0;
 $max = \Drupal::database()->select('flagging', 'f')
   ->fields('f')
   ->condition('f.uid', 0)
   ->countQuery()
   ->execute()->fetchField();
-for ($i = 0; $i <= $max; $i++) {
+while($count <= $max) {
   $flags = $database->select('flagging', 'f')
     ->fields('f', ['id'])
     ->condition('f.uid', 0)
@@ -21,6 +22,6 @@ for ($i = 0; $i <= $max; $i++) {
   $database->delete('flagging')
     ->condition('id', array_keys($flags), 'IN')
     ->execute();
-  $i = $i+$limit;
+  $count = $count + $limit;
 }
 
