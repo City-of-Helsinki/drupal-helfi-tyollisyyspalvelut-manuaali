f11c6e7e3f730380b08e630cb0ab7e830b324fbf
Tapio Pietinsalo <tapio.pietinsalo@citrus.fi>
2026-02-10 11:25:53 +0200

    TP-2110 Update texts and add UI translations

--- public/modules/custom/hel_tpm_general/src/Form/PreventMailForm.php
+++ public/modules/custom/hel_tpm_general/src/Form/PreventMailForm.php
@@ -26,9 +26,7 @@ class PreventMailForm extends FormBase {
   public function buildForm(array $form, FormStateInterface $form_state): array {
     $form['description'] = [
       '#type' => 'markup',
-      '#markup' => $this->t("Use these settings to temporarily prevent sending automatic mails.") . ' ' .
-      $this->t("Importing configurations during deployment does not affect these settings.") . ' ' .
-      $this->t("The settings are stored in database using the State API.") . ' ' .
+      '#markup' => $this->t("Use these settings to temporarily prevent sending automatic emails. Importing configurations during deployment does not affect these settings.") .
       '<p><strong>' . $this->t('Normally these options should not be checked.') . '</strong></p>',
       '#prefix' => '<div class="description">',
       '#suffix' => '</div>',
@@ -38,7 +36,7 @@ class PreventMailForm extends FormBase {
       '#type' => 'checkbox',
       '#title' => $this->t('Block sending mail'),
       '#default_value' => PreventMailUtility::isBlocked(),
-      '#description' => $this->t("Prevents sending all mails that can be altered using the hook_mail_alter() hook."),
+      '#description' => $this->t("Prevents sending all emails that can be altered using the hook_mail_alter() hook."),
     ];
 
     $form['message_templates'] = [
@@ -50,7 +48,7 @@ class PreventMailForm extends FormBase {
       '#type' => 'checkbox',
       '#title' => $this->t('Block mails for ready to publish services'),
       '#default_value' => PreventMailUtility::isReadyToPublishServicesBlocked(),
-      '#description' => $this->t("Prevents sending automatic mails when services are marked as ready to publish. The blocked mails will not be sent again later.") . "<p>" .
+      '#description' => $this->t("Prevents automatic emails from being sent when services are marked as ready to publish. Blocked emails will not be sent at a later time.") . "<p>" .
       $this->t("Affects message template: @template.", [
         '@template' => "group_ready_to_publish_notificat",
       ]) . "</p>",
@@ -60,7 +58,7 @@ class PreventMailForm extends FormBase {
       '#type' => 'checkbox',
       '#title' => $this->t('Block mails for published services'),
       '#default_value' => PreventMailUtility::isPublishedServicesBlocked(),
-      '#description' => $this->t("Prevents sending automatic mails when services are published. The blocked mails will not be sent again later.") . "<p>" .
+      '#description' => $this->t("Prevents automatic emails from being sent when services are published. Blocked emails will not be sent at a later time.") . "<p>" .
       $this->t("Affects message template: @template.", [
         '@template' => "content_has_been_published",
       ]) . "</p>",
@@ -70,7 +68,7 @@ class PreventMailForm extends FormBase {
       '#type' => 'checkbox',
       '#title' => $this->t('Block service update reminder mails'),
       '#default_value' => PreventMailUtility::isUpdateReminderBlocked(),
-      '#description' => $this->t("Prevents sending service reminder mails.") . "<p>" .
+      '#description' => $this->t("Prevents sending service reminder emails.") . "<p>" .
       $this->t("Affects message templates: @template1 and @template2.", [
         '@template1' => "hel_tpm_update_reminder_service",
         '@template2' => "hel_tpm_update_reminder_service2",
@@ -81,7 +79,7 @@ class PreventMailForm extends FormBase {
       '#type' => 'checkbox',
       '#title' => $this->t('Block outdated service mails'),
       '#default_value' => PreventMailUtility::isUpdateReminderOutdatedBlocked(),
-      '#description' => $this->t("Prevents sending mails informing users about outdated services. Services are not outdated until mails are successfully sent.") . "<p>" .
+      '#description' => $this->t("Prevents emails from being sent to inform users about outdated services. Services are not outdated until the emails are successfully sent.") . "<p>" .
       $this->t("Affects message template: @template.", [
         '@template' => "hel_tpm_update_reminder_outdated",
       ]) . "</p>",
@@ -91,7 +89,7 @@ class PreventMailForm extends FormBase {
       '#type' => 'checkbox',
       '#title' => $this->t('Block services missing updaters mails'),
       '#default_value' => PreventMailUtility::isServiceMissingUpdatersBlocked(),
-      '#description' => $this->t("Prevents sending mails informing about missing updaters on services.") . "<p>" .
+      '#description' => $this->t("Prevents emails from being sent to inform users about services with missing updaters.") . "<p>" .
       $this->t("Affects message template: @template.", [
         '@template' => "services_missing_updaters",
       ]) . "</p>",
@@ -101,8 +99,8 @@ class PreventMailForm extends FormBase {
       '#type' => 'checkbox',
       '#title' => $this->t('Block user account expiry mails'),
       '#default_value' => PreventMailUtility::isUserExpirationBlocked(),
-      '#description' => $this->t("Prevents sending mails informing users about expiring user accounts. User expiration does not proceed until mails are successfully sent.") .
-      "<p>" . $this->t("Note: User expiry can be disabled from separate settings page. This options blocks sending mail already in queue, the other option stops adding new user expiry tasks to the queue.") . "</p>" .
+      '#description' => $this->t("Prevents emails from being sent to inform users about expiring user accounts. User expiration does not proceed until the emails are successfully sent.") .
+      "<p>" . $this->t("Note: User expiry can be disabled from a separate settings page. This option blocks emails that are already in the queue, while the other option prevents new user-expiry tasks from being added to the queue.") . "</p>" .
       "<p>" . $this->t("Affects message templates: @template1, @template2 and @template3.", [
         '@template1' => "1st_user_account_expiry_reminder",
         '@template2' => "2nd_user_account_expiry_reminder",
@@ -114,7 +112,7 @@ class PreventMailForm extends FormBase {
       '#type' => 'checkbox',
       '#title' => $this->t('Block mails for deactivated group accounts'),
       '#default_value' => PreventMailUtility::isDeactivatedGroupAccountBlocked(),
-      '#description' => $this->t("Prevents sending mails informing  when deactivating account after removing it from group members. TODO: Services are not outdated until mails are successfully sent.") . "<p>" .
+      '#description' => $this->t("Prevents sending notification emails to users when their account is deactivated because it no longer belongs to any group. The accounts are deactivated regardless of this choice.") . "<p>" .
       $this->t("Affects message template: @template.", [
         '@template' => "hel_tpm_group_account_blocked",
       ]) . "</p>",
