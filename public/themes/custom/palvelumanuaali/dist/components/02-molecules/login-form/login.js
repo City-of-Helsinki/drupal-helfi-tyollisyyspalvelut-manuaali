6a610bff5981e517254c3d1f68e8bf0148884b81
Tapio Pietinsalo <tapio.pietinsalo@citrus.fi>
2026-03-12 12:02:59 +0200

    TP-2158 Update npm packages

--- public/themes/custom/palvelumanuaali/dist/components/02-molecules/login-form/login.js
+++ public/themes/custom/palvelumanuaali/dist/components/02-molecules/login-form/login.js
@@ -1,12 +1,12 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ 149:
-/***/ (function() {
+/***/ 149
+() {
 
 (function(a,b){"use strict";b.behaviors.loginMove={attach:function(){let b=a(".header__primary >.block-user-login-block");if(0!==b.length){let c=a(".main-menu__item--active.main-menu__item--with-sub >.main-menu--sub-1");0===c.length||a(b).appendTo(c)}}}})(jQuery,Drupal,this);
 
-/***/ })
+/***/ }
 
 /******/ 	});
 /************************************************************************/
