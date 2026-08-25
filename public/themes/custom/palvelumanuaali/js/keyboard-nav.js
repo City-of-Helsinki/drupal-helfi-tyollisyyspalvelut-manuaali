343010b066599a3937947953e04122cd0f3dc7c9
Iiro Järvenpää <iirojarvenpaa@CIT-C02DD04TML7M.local>
2026-08-25 09:53:52 +0300

    hotfix select2 keyboard nav

---
+++ public/themes/custom/palvelumanuaali/js/keyboard-nav.js
@@ -0,0 +1,35 @@
+(function ($, Drupal, window) {
+  'use strict';
+
+  Drupal.behaviors.handleKeyboard = {
+    attach: function (context, settings) {
+      selectRemove();
+      makeChoiceRemoversFocusable();
+
+      $(document).on('select2:select', function () {
+        $('.select2-selection__choice__remove').attr('tabindex', '0');
+        $('.select2-selection__choice__remove').attr('aria-label', 'Poista valinta');
+      });
+
+      /**
+       * Pager navigation.
+       */
+      function selectRemove() {
+        $(once('.select2-selection__choice', '.select2-selection__choice__remove', context)).each(function() {
+          $(this).on("keydown",function(e) {
+            if (e.key === "Enter") {
+              e.preventDefault();
+              $(this).trigger('click');
+            }
+          });
+
+        });
+      }
+
+      function makeChoiceRemoversFocusable() {
+        $('.select2-selection__choice__remove').attr('tabindex', '0');
+        $('.select2-selection__choice__remove').attr('aria-label', 'Poista valinta');
+      }
+    }
+  };
+})(jQuery, Drupal, this);
