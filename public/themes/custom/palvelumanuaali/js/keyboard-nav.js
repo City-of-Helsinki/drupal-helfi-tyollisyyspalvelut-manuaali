d5d25542ebc772da71bff77e531473832a95f9dc
Iiro Järvenpää <iirojarvenpaa@CIT-C02DD04TML7M.local>
2026-08-25 11:30:42 +0300

    TP-1457 Generate unique id for form-labels and use it in select2 aria-labelledby

--- public/themes/custom/palvelumanuaali/js/keyboard-nav.js
+++ public/themes/custom/palvelumanuaali/js/keyboard-nav.js
@@ -5,6 +5,7 @@
     attach: function (context, settings) {
       selectRemove();
       makeChoiceRemoversFocusable();
+      addLabeledBy();
 
       $(document).on('select2:select', function () {
         $('.select2-selection__choice__remove').attr('tabindex', '0');
@@ -30,6 +31,14 @@
         $('.select2-selection__choice__remove').attr('tabindex', '0');
         $('.select2-selection__choice__remove').attr('aria-label', 'Poista valinta');
       }
+
+
+      function addLabeledBy() {
+        $(once('.selection', '.select2-selection', context)).each(function() {
+          $(this).attr('aria-labeledby', $(this).parent().parent().parent().siblings('label')[0]['id']);
+        });
+      }
+
     }
   };
 })(jQuery, Drupal, this);
